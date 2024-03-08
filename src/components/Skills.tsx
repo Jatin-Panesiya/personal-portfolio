fa4db6699367962fd55459ad577af06ed6b4b733
Jatin <jatinpanesiya2003@gmail.com>
2024-03-08 17:25:33 +0530

    skills section added

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,33 @@
+import { skills } from "../constants";
 import CardHeading from "./CardHeading";
 
 const Skills = () => {
   return (
     <div className="container my-10">
       <CardHeading title="My Experties" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-5">
+        {skills.map((data, key) => (
+          <div className="bg-[#2a2934] rounded mt-10 py-5 px-5" key={key}>
+            <div className="text-xl text-gray-300  py-3 mb-5">
+              {data.category}
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {data.skills.map((data, key) => (
+                <div
+                  key={key}
+                  className="bg-[#14131a] gap-2 cursor-pointer  rounded-[10px] p-2 flex items-center flex-wrap"
+                  style={{
+                    border: `1px solid ${data.color}`,
+                    color: `${data.color}`,
+                  }}
+                >
+                  <div className="px-2 text-gray-300">{data.name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -33,3 +33,35 @@ export const projectData: projectCard[] = [
     title: "IT Company Portfolio",
   },
 ];
+export const skills = [
+  {
+    category: "Front-end Development",
+    skills: [
+      { name: "HTML", color: "#FFA500" },
+      { name: "CSS", color: "#1572B6" },
+      { name: "Javascript", color: "#FFFF00" },
+      { name: "Typescript", color: "#3178C6" },
+      { name: "React JS", color: "#87CEEB" },
+      { name: "Next JS", color: "#000000" },
+      { name: "Tailwind CSS", color: "#38B2AC" },
+      { name: "Redux Tool Kit", color: "#764ABC" },
+      { name: "MUI", color: "#1572B6" },
+    ],
+  },
+  {
+    category: "Back-end Development",
+    skills: [
+      { name: "NodeJs", color: "#339933" },
+      { name: "Express", color: "#333333" },
+      { name: "MongoDB", color: "#4DB33D" },
+      { name: "Jwt", color: "#764ABC" },
+    ],
+  },
+  {
+    category: "Other",
+    skills: [
+      { name: "Github", color: "#fff" },
+      { name: "Visual Studio Code", color: "#1572B6" },
+    ],
+  },
+];
